3f682f594e0f0513328ad4e8a958614869d77f5b
Michael Fishman <michaeljfishman@gmail.com>
2019-08-19 13:03:10 -0400

    Hardcoded taxi now grounds attributes to specific objects

--- hardcoded_taxi.py
+++
@@ -1,5 +0,0 @@
-from scoping import *
-
-types = ['taxi', 'passenger', 'wall']
-pvariables = ['taxi-x', 'taxi-y', 'passenger-x-cur', 'passenger-y-curr','passenger-in-taxi']
-non_fluents = ['WALL_X', 'WALL_Y','PASSENGER_GOAL_X','PASSENGER_GOAL_Ys']

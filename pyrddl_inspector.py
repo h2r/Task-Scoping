9d91db5adee82b070dc1f58d4f3a893fd2b60ad3
Michael Fishman <michaeljfishman@gmail.com>
2019-08-23 12:54:57 -0400

    Added missing } to switch.rddl. It is still broken

--- pyrddl_inspector.py
+++ pyrddl_inspector.py
@@ -341,8 +341,9 @@ def _compile_aggregation_expression(expr: Expression):
 
 
 if __name__ == '__main__':
-	rddl_file_location = "/home/nishanth/Documents/IPC_Code/rddlsim/files/taxi-rddl-domain/taxi-oo_simple.rddl"
+	# rddl_file_location = "/home/nishanth/Documents/IPC_Code/rddlsim/files/taxi-rddl-domain/taxi-oo_simple.rddl"
 	#rddl_file_location = "./taxi-rddl-domain/taxi-oo_mdp_composite_01.rddl"
+	rddl_file_location = "./misc-domains/switch.rddl"
 	with open(rddl_file_location, 'r') as file:
 		rddl = file.read()
 

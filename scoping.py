ec9d8674651ffb6bab420da4a6e25a6c6ff19faa
Michael Fishman <michaeljfishman@gmail.com>
2020-03-30 17:20:00 -0400

    Fixed effect handling in move_var_from_implied_to_target()

--- scoping.py
+++ scoping.py
@@ -52,18 +52,19 @@ def move_var_from_implied_to_target(skills: List[Skill], vars: List[str]) -> Lis
 	for var in vars:
 		targeting_skills, accidentally_affecting_skills = get_targeting_and_accidentally_affecting_skills(var, skills)
 		for accidental_skill in accidentally_affecting_skills:
-			accidental_skill.effect.append(var)
 			negated_refined_preconditions = []
 			for targeting_skill in targeting_skills:
-				if check_implication(targeting_skill.get_precondition(), accidental_skill.get_precondition()):
+				if targeting_skill.get_action() == accidental_skill.get_action() and \
+						check_implication(targeting_skill.get_precondition(), accidental_skill.get_precondition()):
 					# Add effect to targeting skill
-					# accidental_skill.effect.extend(targeting_skill.get_targeted_variables())
+					targeting_skill.effect.extend(accidental_skill.get_targeted_variables())
 					# Update the accidental_skill's precondition to exclude the targeting skill
 					cond = targeting_skill.get_precondition()
 					if isinstance(cond, ConditionList): #TODO turn negated orlist into andlist of negations
 						cond = cond.to_z3()
 					negated_refined_preconditions.append(z3.Not(cond))
 			accidental_skill.precondition = AndList(accidental_skill.precondition, *negated_refined_preconditions)
+			accidental_skill.effect.append(var)
 			accidental_skill.implicitly_affected_variables.remove(var)
 
 def move_var_from_implied_to_target_test():

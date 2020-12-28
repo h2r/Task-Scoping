fa99b1bdf46494c812c1415391a0bb07aa48d468
Michael Fishman <michaeljfishman@protonmail.com>
2020-12-28 18:57:21 -0500

    Reverted a minecraft pddl file. Other stuff?

--- profiling/minecraft_profile.py
+++ profiling/minecraft_profile.py
@@ -35,46 +35,27 @@ if __name__ == '__main__':
     start_time = time.time()
     domain = f"{domains_dir}/minecraft2/minecraft-contrived2.pddl"
     # problems = ["domains/minecraft2/prob_obsidian_with_pick.pddl", "domains/minecraft2/prob_obsidian_without_pick.pddl"]
-    problems = [
-        # "domains/minecraft2/prob_irrel_obsidian_with_pick.pddl"
-        # "domains/minecraft2/prob_irrel_flint_with_pick.pddl"
-        f"{domains_dir}/minecraft2/prob_irrel_nether_with_pick.pddl"
-    ]
+    problem = f"{domains_dir}/minecraft2/prob_irrel_nether_with_pick.pddl"
 
-    for problem in problems:
-        print(f"Domain: {domain}")
-        print(f"Problem: {problem}")
-        # Path to save entire, non-human-readable profile object
-        # profile_path = f'time_profiles/minecraft_old'
-        profile_path = get_profile_path(domain, problem, "_nonverbose")
-        # Path to save human-readable profile stats
-        profile_path_txt = profile_path + ".txt"
-        print(f"Human-readable profile output: {profile_path_txt}")
-        # Create folder containing profile path, if it does not yet exist
-        make_dir(profile_path, is_file=True)
-        # Run the profiler and save the full, non-human-readable results
-        cProfile.run('scope_pddl(domain, problem, verbose=-1)', profile_path)
-        # Get the file object for outputting human-readable profile stats
-        stats_readable_file = open(profile_path_txt,"w")
-        # Get stats object from saves profile, and set it to stream output to stats_readable_file
-        p = pstats.Stats(profile_path, stream = stats_readable_file)
-        # Save output to stats_reasable_file
-        p.strip_dirs().sort_stats(SortKey.CUMULATIVE).print_stats()
-        # scope_pddl(domain, problem)
-
-
-        # # Path to save entire, non-human-readable profile object
-        # profile_path = f'time_profiles/minecraft_new'
-        # # Path to save human-readable profile stats
-        # profile_path_txt = profile_path + ".txt"
-        # cProfile.run('scope_pddl(domain, problem, old_var_uniquify = False)', profile_path)
-        # # Get the file object for outputting human-readable profile stats
-        # stats_readable_file = open(profile_path_txt,"w")
-        # # Get stats object from saves profile, and set it to stream output to stats_readable_file
-        # p = pstats.Stats(profile_path, stream = stats_readable_file)
-        # # Save output to stats_reasable_file
-        # p.strip_dirs().sort_stats(SortKey.CUMULATIVE).print_stats()
-        # # scope_pddl(domain, problem)
+    print(f"Domain: {domain}")
+    print(f"Problem: {problem}")
+    # Path to save entire, non-human-readable profile object
+    # profile_path = f'time_profiles/minecraft_old'
+    profile_path = get_profile_path(domain, problem, "_nonverbose")
+    # Path to save human-readable profile stats
+    profile_path_txt = profile_path + ".txt"
+    print(f"Human-readable profile output: {profile_path_txt}")
+    # Create folder containing profile path, if it does not yet exist
+    make_dir(profile_path, is_file=True)
+    # Run the profiler and save the full, non-human-readable results
+    cProfile.run('scope_pddl(domain, problem, verbose=-1)', profile_path)
+    # Get the file object for outputting human-readable profile stats
+    stats_readable_file = open(profile_path_txt,"w")
+    # Get stats object from saves profile, and set it to stream output to stats_readable_file
+    p = pstats.Stats(profile_path, stream = stats_readable_file)
+    # Save output to stats_reasable_file
+    p.strip_dirs().sort_stats(SortKey.CUMULATIVE).print_stats()
+    # scope_pddl(domain, problem)
 
     end_time = time.time()
     print(f"Total time: {end_time - start_time}")

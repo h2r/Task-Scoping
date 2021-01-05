6e29e6a908d41d12dfc159cd85c127f2b787f8ef
NishanthJKumar <nishanth.kumar20@gmail.com>
2021-01-05 11:04:04 -0500

    monkeys_pddl prob-02 domain: scoping + FD is faster than plain FD with blind heuristic

--- oo_scoping/examples/domains/multi_monkeys_playroom_strips/generate_adjacency_init_conds.py
+++ oo_scoping/examples/domains/multi_monkeys_playroom_strips/generate_adjacency_init_conds.py
@@ -1,9 +1,18 @@
+object_definition_str_x = ''
+object_definition_str_y = ''
 init_conds_str = ''
-grid_limit = 10
+grid_limit = 20
 for x1 in range(1,grid_limit):
+    object_definition_str_x += f"x{x1} "
     init_conds_str += f"(adjacent-x x{x1} x{x1+1})\n(adjacent-x x{x1+1} x{x1})\n"
 
 for y1 in range(1,grid_limit):
+    object_definition_str_y += f"y{y1} "
     init_conds_str += f"(adjacent-y y{y1} y{y1+1})\n(adjacent-y y{y1+1} y{y1})\n"
-    
+
+object_definition_str_x += f"x{grid_limit}"
+object_definition_str_y += f"y{grid_limit}"
+
+print(object_definition_str_x)
+print(object_definition_str_y)
 print(init_conds_str)

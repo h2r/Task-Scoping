452f340c899021a66bd2b9d58a48f1436274b8af
Michael Fishman <michaeljfishman@protonmail.com>
2021-09-10 18:07:11 -0500

    echo

--- ubuntu_scripts/get_enhsp_docker.sh
+++ ubuntu_scripts/get_enhsp_docker.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 docker pull aiplanning/planutils
 docker run --privileged -v /etc/localtime:/etc/localtime -v /home/ubuntu/ubuntu_scripts/mnt_wrap:/mnt/ -it aiplanning/planutils bash
+echo "loaded planutils container"

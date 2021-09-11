5be98ea111f7a86e7f8f4b2cdef95d846f64fa99
Michael Fishman <michaeljfishman@protonmail.com>
2021-09-11 08:00:11 -0500

    typo fix

--- ubuntu_scripts/config_all.sh
+++ ubuntu_scripts/config_all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-declare -a arr=("do_apt_get.sh" "config_aws.sh" "config_git.sh" "config_conda.sh" "instal_fast_downward.sh")
+declare -a arr=("do_apt_get.sh" "config_aws.sh" "config_git.sh" "config_conda.sh" "install_fast_downward.sh")
 # Save the working directory. We will come back here after sourcing each script
 cd /mnt/scoping
 cwd=$(pwd)

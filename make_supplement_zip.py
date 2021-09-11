132246825da114c213d48fda581d60703ec57426
Michael Fishman <michaeljfishman@protonmail.com>
2021-09-11 07:56:23 -0500

    Script to make supplement zip

---
+++ make_supplement_zip.py
@@ -0,0 +1,29 @@
+import os
+from shutil import copytree, ignore_patterns
+import shutil
+from zipfile import ZipFile
+import os
+
+
+
+ign_pats = [".git*",".gitignore", "oo_scoping.egg-info/*", ".vscode", ".ds_store", "ignore*"]
+repo_root = os.path.dirname(__file__)
+repo_parent = os.path.dirname(repo_root)
+tgt_dir = os.path.join(repo_parent, "scoping_supplement")
+tgt_dir_repo = os.path.join(tgt_dir, "scoping")
+
+if os.path.isdir(tgt_dir):
+    shutil.rmtree(tgt_dir)
+
+
+
+copytree(repo_root, tgt_dir_repo, ignore=ignore_patterns(*ign_pats))
+
+supplement_pdf_pth = os.path.join(repo_parent, "Scoping_AAAI_2022_Supplement.pdf")
+shutil.copy(supplement_pdf_pth, tgt_dir)
+
+archive_pth = os.path.join(repo_parent, "scoping_supplement")
+archive_pth_with_ext = os.path.join(archive_pth, "zip")
+if os.path.isfile(archive_pth_with_ext):
+    os.remove(archive_pth_with_ext)
+shutil.make_archive(archive_pth, format="zip", root_dir=tgt_dir, base_dir=".")

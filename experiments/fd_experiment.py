418f7f248acf2de3587b80bead55bdcb56c7f7f4
Michael Fishman <michaeljfishman@protonmail.com>
2023-01-23 20:03:51 -0500

    Fixed h2. It's slow.

--- experiments/fd_experiment.py
+++ experiments/fd_experiment.py
@@ -261,7 +261,8 @@ def translate_and_scope(domain, problem, unscoped_sas_path):
 
 SEARCH_CONFIGS = {
     "lmcut":"astar(lmcut())",
-    "ms":"astar(merge_and_shrink(shrink_strategy=shrink_bisimulation(greedy=false),merge_strategy=merge_sccs(order_of_sccs=topological,merge_selector=score_based_filtering(scoring_functions=[goal_relevance,dfp,total_order])),label_reduction=exact(before_shrinking=true,before_merging=false),max_states=50k,threshold_before_merge=1))"
+    "ms":"astar(merge_and_shrink(shrink_strategy=shrink_bisimulation(greedy=false),merge_strategy=merge_sccs(order_of_sccs=topological,merge_selector=score_based_filtering(scoring_functions=[goal_relevance,dfp,total_order])),label_reduction=exact(before_shrinking=true,before_merging=false),max_states=50k,threshold_before_merge=1))",
+    "h2":"astar(hm(m=2, verbosity=normal, transform=no_transform(), cache_estimates=true))"
 }
 
 def plan(sas_path, fd_path, plan_type: str = "lmcut"):

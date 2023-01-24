c8f67d5bb5ab8205c50ffc55f384cde92d000479
Michael Fishman <michaeljfishman@protonmail.com>
2023-01-23 19:09:54 -0500

    Anonymized and removed old files

--- setup.py
+++ setup.py
@@ -10,9 +10,9 @@ setup(name='oo_scoping',
       version='1.0',
       description='Object-Oriented state abstraction',
       long_description=(here / 'README.md').read_text(encoding='utf-8'),
-      author='Michael Fishman and Nishanth Kumar',
-      author_email='michaeljfishman@protonmail.com, nishanth.kumar20@gmail.com',
-      url='https://github.com/h2r/OO-Scoping-IPC',
+      author='Anonymous',
+      author_email='Anonymous',
+      url='Anonymous',
       install_requires=[
           'z3-solver'
       ],

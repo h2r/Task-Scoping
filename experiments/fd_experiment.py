a38932c756e43d2e3958a7fe327194c6f9fe7640
Michael Fishman <michaeljfishman@protonmail.com>
2023-01-23 20:23:37 -0500

    Updated path in fd_experiment.py

--- experiments/fd_experiment.py
+++ experiments/fd_experiment.py
@@ -248,13 +248,13 @@ def save_cmd_output(cmd_output, save_dir):
 
 
 def translate(domain, problem, sas_path):
-    cmd_pieces = ["python", f"{repo_root}/downward_translate/translate_and_scope.py", domain, problem, "--sas-file", sas_path]
+    cmd_pieces = ["python", f"{repo_root}/oo_scoping/downward_translate/translate_and_scope.py", domain, problem, "--sas-file", sas_path]
     cmd_output = subprocess.run(cmd_pieces, capture_output=True, shell=False)
     return cmd_output
 
 
 def translate_and_scope(domain, problem, unscoped_sas_path):
-    cmd_pieces = ["python", f"{repo_root}/downward_translate/translate_and_scope.py", domain, problem, "--sas-file", unscoped_sas_path, "--scope", "True"]
+    cmd_pieces = ["python", f"{repo_root}/oo_scoping/downward_translate/translate_and_scope.py", domain, problem, "--sas-file", unscoped_sas_path, "--scope", "True"]
     cmd_output = subprocess.run(cmd_pieces, capture_output=True, shell=False)
     return cmd_output
 
